4e30768d3488577e374339b45e86e1f2445eba5c
magicsk <magicsk@magicsk.eu>
2022-07-14 17:06:00 +0200

    reset, lang fix

--- src/locale.ts
+++ src/locale.ts
@@ -1,5 +1,14 @@
-const localeDirectory = import.meta.globEager('./compiled-lang/*.json')
+import en from './compiled-lang/en.json'
+import sk from './compiled-lang/sk.json'
 
+const localeDirectory = {
+  './compiled-lang/en.json': {
+    default: en,
+  },
+  './compiled-lang/sk.json': {
+    default: sk,
+  },
+}
 const gatherLocales = Object.keys(localeDirectory).reduce(
   (gatherLocales, file) => {
     const locale = file.substring(
